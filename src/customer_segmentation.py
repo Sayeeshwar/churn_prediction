8d3e6bdc0e8370a43834e5a6eea9b3acbe3a5cda
Sayeeshwar <sayeeshwar.girish@gmail.com>
2025-04-10 07:18:22 +0100

    Add code for segmentation of customers

--- src/customer_segmentation.py
+++ src/customer_segmentation.py
@@ -1,5 +1,21 @@
-def load_model():
-    return
+import pickle
 
-def segment_customers():
-    return
+def load_model(model_path):
+    with open(model_path, 'rb') as file:
+        model = pickle.load(file)
+    return model
+
+def segment_customers(df, model, features):
+    print('features', features)
+    churn_proba = model.predict_proba(df[features])[:, 1]
+    df['churn_probability'] = churn_proba
+    
+    high_risk = df[df['churn_probability'] > 0.7]
+    medium_risk = df[(df['churn_probability'] > 0.4) & (df['churn_probability'] <= 0.7)]
+    low_risk = df[df['churn_probability'] <= 0.4]
+    
+    return {
+        'high_risk': high_risk,
+        'medium_risk': medium_risk,
+        'low_risk': low_risk
+    }
